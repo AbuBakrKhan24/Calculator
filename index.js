116b9a27e905ffe0e99135ff4d5b064cff449fb3
AbuBakrKhan24 <abubakrkhan224@gmail.com>
2022-06-08 10:32:06 +0200

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+let sum = " ";
+
+function buildSum(char) {
+  sum += char;
+  document.innerHTML = sum;
+  document.getElementById("output").innerHTML += char;
+}
+
+function calculate() {
+  sum = eval(sum);
+  document.getElementById("output").innerHTML = sum;
+}
+
+function reset(clear) {
+  console.log(" ");
+  document.getElementById("output").innerHTML = " ";
+}
